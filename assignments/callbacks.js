5bbd0f2d724f6b775a02248a485636d826c39959
karapeoples <52808542+karapeoples@users.noreply.github.com>
2019-10-07 17:57:28 -0500

    Fixed my Closures

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,102 +1,117 @@
-//* Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
+//@ Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ["Pencil", "Notebook", "yo-yo", "Gum"];
 
+//@ GIVEN THIS PROBLEM:
 
+function firstItem(arr, cb) {
+	//@ firstItem passes the first item of the given array to the callback function.
+}
 
-  //* GIVEN THIS PROBLEM:
+//@ SOLUTION:
 
-  function firstItem(arr, cb) {
-    //* firstItem passes the first item of the given array to the callback function.
-  }
+function firstItem(arr, cb) {
+	return cb(arr[0]);
+}
 
-  //* SOLUTION:
+//@ NOTES ON THE SOLUTION:
 
-  function firstItem(arr, cb) {
-    return cb(arr[0]);
-  }
+//@ firstItem is a higher order function.
+//@ It expects a callback (referred to as `cb`) as its second argument.
+//@ To test our solution, we can use the given `items` array and a variety of callbacks.
+//@ Note how callbacks can be declared separately, or inlined.
 
-  //* NOTES ON THE SOLUTION:
+//@ TEST 1 (inlined callback):
 
-  //* firstItem is a higher order function.
-  //* It expects a callback (referred to as `cb`) as its second argument.
-  //* To test our solution, we can use the given `items` array and a variety of callbacks.
-  //* Note how callbacks can be declared separately, or inlined.
+const test1 = firstItem(items, item => `I love my ${item}!`);
+console.log(test1); //# "I love my Pencil!"
 
-  //* TEST 1 (inlined callback):
+//@ TEST 2 (declaring callback before hand):
 
-  const test1 = firstItem(items, item => `I love my ${item}!`);
-  console.log(test1); //* "I love my Pencil!"
+function logExorbitantPrice(article) {
+	return `this ${article} is worth a million dollars!`;
+}
 
-  //* TEST 2 (declaring callback before hand):
-
-  function logExorbitantPrice(article) {
-    return `this ${article} is worth a million dollars!`;
-  };
-
-  const test2 = firstItem(items, logExorbitantPrice);
-  console.log(test2); //* "this Pencil is worth a million dollars!"
-
-
+const test2 = firstItem(items, logExorbitantPrice);
+console.log(test2); //#  "this Pencil is worth a million dollars!"
 
 function getLength(arr, cb) {
-   return cb(arr);
-  //* getLength passes the length of the array into the callback.
+	return cb(arr);
+	//@ getLength passes the length of the array into the callback.
 }
 
-let size = (arr) => arr.length;
-console.log(getLength(items, size))
-
+let size = arr => arr.length;
+console.log(getLength(items, size));
 
 function last(arr, cb) {
-  return cb(arr);
-  //* last passes the last item of the array into the callback.
+	return cb(arr);
+	//@ last passes the last item of the array into the callback.
 }
-let lastItem = (arr) => arr.slice(-1)[0];;
+let lastItem = arr => arr.slice(-1)[0];
 
-console.log(last(items,lastItem))
+console.log(last(items, lastItem));
 
 function sumNums(x, y, cb) {
-  return cb (x,y);
-  //* sumNums adds two numbers (x, y) and passes the result to the callback.
+	return cb(x, y);
+	//@  sumNums adds two numbers (x, y) and passes the result to the callback.
 }
 
-let add = (x,y) => x + y
+let add = (x, y) => x + y;
 
-console.log (sumNums(2,4, add))
+console.log(sumNums(2, 4, add));
 
 function multiplyNums(x, y, cb) {
-  return cb (x,y);
-  //* multiplyNums multiplies two numbers and passes the result to the callback.
+	return cb(x, y);
+	//@ multiplyNums multiplies two numbers and passes the result to the callback.
 }
-let multiply = (x,y) => x * y
+let multiply = (x, y) => x * y;
 
-console.log (multiplyNums(2,4, multiply))
+console.log(multiplyNums(2, 4, multiply));
 
 function contains(item, list, cb) {
-  return cb(item, list)
-  //* contains checks if an item is present inside of the given array/list.
-  //* Pass true to the callback if it is, otherwise pass false.
+	return cb(item, list);
+	//@ contains checks if an item is present inside of the given array/list.
+	//@ Pass true to the callback if it is, otherwise pass false.
 }
-let isHere = function(item, list){
-  if (list.includes (item)){
-    return true
-  }
-    else{
-      return false
-    }
-  }
+let isHere = function(item, list) {
+	return list.includes(item);
+};
 
-    console.log(contains ('yo-yo', items, isHere));
+console.log(contains("yo-yo", items, isHere));
 
+//@!STRETCH PROBLEM
 
-//!STRETCH PROBLEM 
-
+const duplicates = [
+	"yellow",
+	"yellow",
+	"blue",
+	"red",
+	"purple",
+	"purple",
+	"green",
+	"blue",
+	"magenta",
+	"cyan",
+];
+const colors = [];
 function removeDuplicates(array, cb) {
-  return cb(array)
-  //* removeDuplicates removes all duplicate values from the given array.
-  //* Pass the duplicate free array to the callback function.
-  //* Do not mutate the original array.
+	return cb(array);
+	//@  removeDuplicates removes all duplicate values from the given array.
+	//@ Pass the duplicate free array to the callback function.
+	//@ Do not mutate the original array.
 }
 
- 
+function unique() {
+	let colors = [];
+	return duplicates.filter(function(noDuplicates) {
+		var k = noDuplicates;
+		return colors.indexOf(k) >= 0 ? false : colors.push(k);
+	});
+}
+console.log(removeDuplicates(duplicates, unique));
+
+//! this is a comment
+//@ this is a comment
+//# this is a comment
+//$ this is a comment
+//% this is a comment
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,47 +1,67 @@
-//* ==== Challenge 1: Write your own closure ====
-//* Write a closure of your own creation.
-//* Keep it simple! Remember a closure is just a function
-//* that manipulates variables defined in the outer scope.
-//* The outer scope can be a parent function, or the top level of the script.
-function sandwich (layer1){
-  console.log (layer1);
-}
-sandwich("The Bread Goes on Top & Bottom");
-  function hamSandwich (layer2){
-    console.log (layer2);
-  }
-  hamSandwich("Ham Goes in the Middle");
-    function hamwCheese(layer3){
-      console.log (layer3)
-    }
-    hamwCheese("Add Cheese between the bread...Enjoy!");
+//@ ==== Challenge 1: Write your own closure ====
+//@ Write a closure of your own creation.
+//@ Keep it simple! Remember a closure is just a function
+//@ that manipulates variables defined in the outer scope.
+//@ The outer scope can be a parent function, or the top level of the script.
+      
+  
+function sandwichbottom (){
+    let layer1 = (`Bread`);
+      console.log (`What goes on bottom? ${layer1}`);
+  
+  function hamSandwich (){
+    let layer2 = (`Ham`)
+    let layer3 = (`Cheese and`) 
+    console.log (`What goes inside? ${layer2}`);
 
-
-
+    function sandwich(){ 
+      console.log (`What goes on the top of the Ham? ${layer3} more ${layer1}`);
+     
+       
+      };
+       sandwich(); 
+     };
+     hamSandwich();
+  };
+  sandwichbottom();
 
 //! STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
+//#Also Included IIFE
 
-//* ==== Challenge 2: Implement a "counter maker" function ====
-const counterMaker = () => {
-  //* IMPLEMENTATION OF counterMaker:
-  //* 1- Declare a `count` variable with a value of 0. We will be mutating it, so declare it using `let`!
-  //* 2- Declare a function `counter`. It should increment and return `count`.
-  //*      NOTE: This `counter` function, being nested inside `counterMaker`,
-  //*      "closes over" the `count` variable. It can "see" it in the parent scope!
-  //* 3- Return the `counter` function.
-};
-//* Example usage: const myCounter = counterMaker();
-//* myCounter(); //* 1
-//* myCounter(); //* 2
 
-//* ==== Challenge 3: Make `counterMaker` more sophisticated ====
-//* It should have a `limit` parameter. Any counters we make with `counterMaker`
-//* will refuse to go over the limit, and start back at 1.
+//@ ==== Challenge 2: Implement a "counter maker" function ====
+//@ IMPLEMENTATION OF counterMaker:
+  //@ 1- Declare a `count` variable with a value of 0. We will be mutating it, so declare it using `let`!
+  //@ 2- Declare a function `counter`. It should increment and return `count`.
+  //@      NOTE: This `counter` function, being nested inside `counterMaker`,
+  //@      "closes over" the `count` variable. It can "see" it in the parent scope!
+  //@ 3- Return the `counter` function.
 
-//* ==== Challenge 4: Create a counter function with an object that can increment and decrement ====
-const counterFactory = () => {
-  //* Return an object that has two methods called `increment` and `decrement`.
-  //* `increment` should increment a counter variable in closure scope and return it.
-  //* `decrement` should decrement the counter variable and return it.
-};
+ /*  const counterMaker = () => {
+    function counter() { 
+    while (count < 10)
+    count += "The number is " + count;
+    count++
+  }();
+  let count = 0
+  }; 
+
+
+ console.log(counterMaker); */
+
+//@ Example usage: const myCounter = counterMaker();
+//@ myCounter(); //@ 1
+//@ myCounter(); //@ 2
+
+//@ ==== Challenge 3: Make `counterMaker` more sophisticated ====
+//@ It should have a `limit` parameter. Any counters we make with `counterMaker`
+//@ will refuse to go over the limit, and start back at 1.
+
+//@ ==== Challenge 4: Create a counter function with an object that can increment and decrement ====
+
+  //@ Return an object that has two methods called `increment` and `decrement`.
+  //@ `increment` should increment a counter variable in closure scope and return it.
+  //@ `decrement` should decrement the counter variable and return it.
+  /* const counterFactory = () => {
+}; */
